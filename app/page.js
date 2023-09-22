dd4a950550bd83890fa0c2eedc05b2176b2abcf4
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-09-22 19:41:48 +0300

    main page design

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import background from '../public/background.webp';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
+    <main
+      className={styles.main}
+      style={{
+        backgroundImage: `url(background.webp)`,
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className={styles.contentContainer}>
+        <img className={styles.gameLogo} src="/game_logo.png" />
+        <div className={styles.playButtonContainer}>
+          <button className={styles.playButton}>Play</button>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
